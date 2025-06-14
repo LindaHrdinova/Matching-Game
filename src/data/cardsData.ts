e67c35de0d4f8c10c418f6a024cdd368c42c7ea5
Linda Hrdinová <hrlin@seznam.cz>
2025-06-14 16:21:31 +0200

    Reorganisation ReactIcon import, SQL -> 11ty

--- src/data/cardsData.ts
+++ src/data/cardsData.ts
@@ -1,14 +1,13 @@
 import { IconType } from 'react-icons';
-import { SiCss3 } from 'react-icons/si';
-import { FaReact } from 'react-icons/fa';
-import { SiJavascript } from 'react-icons/si';
-import { FaHtml5 } from 'react-icons/fa';
-import { SiSpring } from 'react-icons/si';
-import { FaJava } from 'react-icons/fa';
-import { SiTypescript } from 'react-icons/si';
-import { BsFiletypeSql } from 'react-icons/bs';
+import {
+  SiCss3,
+  SiJavascript,
+  SiSpring,
+  SiTypescript,
+  SiEleventy,
+} from 'react-icons/si';
+import { FaReact, FaHtml5, FaJava, FaSass } from 'react-icons/fa';
 import { RiTailwindCssFill } from 'react-icons/ri';
-import { FaSass } from 'react-icons/fa';
 
 export const cardList: { Icon: IconType; color: string }[] = [
   { Icon: SiCss3, color: '#264de4' },
@@ -19,7 +18,7 @@ export const cardList: { Icon: IconType; color: string }[] = [
   { Icon: SiSpring, color: '#6DB33F' },
   { Icon: FaJava, color: '#5382a1' },
   { Icon: FaHtml5, color: '#E34F26' },
-  { Icon: BsFiletypeSql, color: '#000000' },
+  { Icon: SiEleventy, color: '#000000' },
   { Icon: SiJavascript, color: '#F7DF1E' },
   { Icon: SiCss3, color: '#264de4' },
   { Icon: SiTypescript, color: '#3178C6' },
@@ -29,6 +28,6 @@ export const cardList: { Icon: IconType; color: string }[] = [
   { Icon: SiSpring, color: '#6DB33F' },
   { Icon: FaJava, color: '#5382a1' },
   { Icon: FaHtml5, color: '#E34F26' },
-  { Icon: BsFiletypeSql, color: '#000000' },
+  { Icon: SiEleventy, color: '#000000' },
   { Icon: SiJavascript, color: '#F7DF1E' },
 ];
